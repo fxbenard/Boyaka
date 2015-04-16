790affdbd8e71d8090bfb275404540240ef6f1cd
fxbenard <fxbenard@gmail.com>
2015-04-16 15:32:11 +0200

    Sample in da place

---
+++ fxb-sample-translations.php
@@ -0,0 +1,245 @@
+<?php
+/**
+ * Plugin Name: FxB Sample Translations
+ * Plugin URI: https://fxbenard.com/
+ * Description: Translations for the FxB Sample plugin
+ * Version: 1.0.0
+ * Author: fxbenard
+ * Author URI: https://fxbenard.com/
+ * Text Domain: fxb-sample-translations
+ * Domain Path: languages
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright Copyright (c) 2015, fxbenard
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'FXB_SAMPLE_Plugin' ) ) {
+
+	/**
+	 * Main FXB_SAMPLE_Plugin class
+	 *
+	 * @since       1.0.0
+	 */
+	class FXB_SAMPLE_Plugin {
+
+		/**
+		 *
+		 *
+		 * @var         FXB_SAMPLE_Plugin $instance The one true FXB_SAMPLE_Plugin
+		 * @since       1.0.0
+		 */
+		private static $instance;
+
+
+		/**
+		 * Get active instance
+		 *
+		 * @access      public
+		 * @since       1.0.0
+		 * @return      object self::$instance The one true FXB_SAMPLE_Plugin
+		 */
+		public static function instance() {
+			if ( !self::$instance ) {
+				self::$instance = new FXB_SAMPLE_Plugin();
+				self::$instance->setup_constants();
+				self::$instance->includes();
+				self::$instance->load_textdomain();
+				self::$instance->hooks();
+
+				// retrieve our license key from the DB
+				$license_key = trim( get_option( 'fxb_sample_license_key' ) );
+
+				// setup the updater
+				$edd_updater = new FXB_SAMPLE_Plugin_Updater( FXB_SAMPLE_STORE_URL, __FILE__, array(
+						'version' 	=> FXB_SAMPLE_TRANSLATIONS_VER,
+						'license' 	=> $license_key, 		// license key (used get_option above to retrieve from DB)
+						'item_name' => FXB_SAMPLE_ITEM_NAME,
+						'author' 	=> 'fxbenard',
+					)
+				);
+			}
+
+			return self::$instance;
+		}
+
+
+		/**
+		 * Setup plugin constants
+		 *
+		 * @access      private
+		 * @since       1.0.0
+		 * @return      void
+		 */
+		private function setup_constants() {
+			// Plugin version
+			define( 'FXB_SAMPLE_TRANSLATIONS_VER', '1.0.0' );
+
+			// Plugin path
+			define( 'FXB_SAMPLE_TRANSLATIONS_DIR', plugin_dir_path( __FILE__ ) );
+
+			// Plugin URL
+			define( 'FXB_SAMPLE_TRANSLATIONS_URL', plugin_dir_url( __FILE__ ) );
+
+			define( 'FXB_SAMPLE_STORE_URL', 'https://fxbenard.com' );
+
+			define( 'FXB_SAMPLE_ITEM_NAME', 'Sample' );
+		}
+
+
+		/**
+		 * Include necessary files
+		 *
+		 * @access      private
+		 * @since       1.0.0
+		 * @return      void
+		 */
+		private function includes() {
+			// Include scripts
+			//require_once FXB_SAMPLE_TRANSLATIONS_DIR . 'includes/scripts.php';
+			require_once FXB_SAMPLE_TRANSLATIONS_DIR . 'includes/functions.php';
+
+			/**
+			 *
+			 *
+			 * @todo        The following files are not included in the boilerplate, but
+			 *              the referenced locations are listed for the purpose of ensuring
+			 *              path standardization in EDD extensions. Uncomment any that are
+			 *              relevant to your extension, and remove the rest.
+			 */
+			// require_once FXB_SAMPLE_TRANSLATIONS_DIR . 'includes/shortcodes.php';
+			// require_once FXB_SAMPLE_TRANSLATIONS_DIR . 'includes/widgets.php';
+		}
+
+
+		/**
+		 * Run action and filter hooks
+		 *
+		 * @access      private
+		 * @since       1.0.0
+		 * @return      void
+		 *
+		 * @todo        The hooks listed in this section are a guideline, and
+		 *              may or may not be relevant to your particular extension.
+		 *              Please remove any unnecessary lines, and refer to the
+		 *              WordPress codex and EDD documentation for additional
+		 *              information on the included hooks.
+		 *
+		 *              This method should be used to add any filters or actions
+		 *              that are necessary to the core of your extension only.
+		 *              Hooks that are relevant to meta boxes, widgets and
+		 *              the like can be placed in their respective files.
+		 *
+		 *              IMPORTANT! If you are releasing your extension as a
+		 *              commercial extension in the EDD store, DO NOT remove
+		 *              the license check!
+		 */
+		private function hooks() {
+			// Handle licensing
+			if ( ! class_exists( 'FXB_SAMPLE_Plugin_Updater' ) ) {
+				// load our custom updater
+				include dirname( __FILE__ ) . '/includes/FXB_SAMPLE_Plugin_Updater.php';
+			}
+		}
+
+		/**
+		 * Internationalization
+		 *
+		 * @access      public
+		 * @since       1.0.0
+		 * @return      void
+		 */
+		public function load_textdomain() {
+
+			// Set filter for language directory
+			$lang_dir = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
+			$lang_dir = apply_filters( 'fxb_sample_french_languages_directory', $lang_dir );
+
+			// Traditional WordPress plugin locale filter
+			$locale = apply_filters( 'plugin_locale', get_locale(), 'sample-french' );
+			$mofile = sprintf( '%1$s-%2$s.mo', 'sample-french', $locale );
+
+			// Setup paths to current locale file
+			$mofile_local   = $lang_dir . $mofile;
+			$mofile_global  = WP_LANG_DIR . '/sample-french/' . $mofile;
+
+			if ( file_exists( $mofile_global ) ) {
+				// Look in global /wp-content/languages/sample-french/ folder
+				load_textdomain( 'sample-french', $mofile_global );
+			} elseif ( file_exists( $mofile_local ) ) {
+				// Look in local /wp-content/plugins/sample-french/languages/ folder
+				load_textdomain( 'sample-french', $mofile_local );
+			} else {
+				// Load the default language files
+				load_plugin_textdomain( 'sample-french', false, $lang_dir );
+			}
+		}
+
+		public function settings( $settings ) {
+			$new_settings = array(
+				array(
+					'id'    => 'fxb_sample_plugin_settings',
+					'name'  => '<strong>' . __( 'FXB SAMPLE Settings', 'sample-french' ) . '</strong>',
+					'desc'  => __( 'Configure FXB SAMPLE Settings', 'sample-french' ),
+					'type'  => 'header',
+				),
+			);
+
+			return array_merge( $settings, $new_settings );
+		}
+	}
+} // End if class_exists check
+
+
+/**
+ * The main function responsible for returning the one true FXB_SAMPLE_Plugin
+ * instance to functions everywhere
+ *
+ * @since       1.0.0
+ * @return      \FXB_SAMPLE_Plugin The one true FXB_SAMPLE_Plugin
+ *
+ * @todo        Inclusion of the activation code below isn't mandatory, but
+ *              can prevent any number of errors, including fatal errors, in
+ *              situations where your extension is activated but EDD is not
+ *              present.
+ */
+function FXB_SAMPLE_Plugin_load() {
+	if ( ! class_exists( 'Easy_Digital_Downloads' ) ) {
+		if ( ! class_exists( 'EDD_Extension_Activation' ) ) {
+			require_once 'includes/class.extension-activation.php';
+		}
+
+		$activation = new EDD_Extension_Activation( plugin_dir_path( __FILE__ ), basename( __FILE__ ) );
+		$activation = $activation->run();
+		return FXB_SAMPLE_Plugin::instance();
+	} else {
+		return FXB_SAMPLE_Plugin::instance();
+	}
+}
+add_action( 'plugins_loaded', 'FXB_SAMPLE_Plugin_load' );
+
+/**
+ * The activation hook is called outside of the singleton because WordPress doesn't
+ * register the call from within the class, since we are preferring the plugins_loaded
+ * hook for compatibility, we also can't reference a function inside the plugin class
+ * for the activation function. If you need an activation function, put it here.
+ *
+ * @since       1.0.0
+ * @return      void
+ */
+function FXB_SAMPLE_Plugin_activation() {
+	/* Activation functions here */
+
+}
+register_activation_hook( __FILE__, 'FXB_SAMPLE_Plugin_activation' );
--- includes/functions.php
+++ includes/functions.php
@@ -2,7 +2,7 @@
 /**
  * Helper Functions
  *
- * @package     FxB\Bloom French\Functions
+ * @package     FxB\Sample Translations\Functions
  * @since       1.0.0
  */
 
@@ -12,28 +12,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if(!function_exists('fxb_license_menu')) {
+if ( ! function_exists( 'fxb_license_menu' ) ) {
 	function fxb_license_menu() {
-		add_plugins_page( __( 'FxB trads', 'bloom-french' ), __( 'FxB trads', 'bloom-french' ), 'manage_options', 'fbx-trads-license', 'fxb_license_page' );
+		add_plugins_page( __( 'FxB trads', 'fxb-sample-translations' ), __( 'FxB trads', 'fxb-sample-translations' ), 'manage_options', 'fbx-trads-license', 'fxb_license_page' );
 	}
 	add_action( 'admin_menu', 'fxb_license_menu' );
 }
 
-if(!function_exists('fxb_license_page')) {
+if ( ! function_exists( 'fxb_license_page' ) ) {
 	function fxb_license_page() {
-		$license  = get_option( 'fxb_bloom_license_key' );
-		$status   = get_option( 'fxb_bloom_license_status' );
+		$license  = get_option( 'fxb_sample_license_key' );
+		$status   = get_option( 'fxb_sample_license_status' );
 		?>
 		<div class="wrap">
-			<h2><?php _e( 'Bloom French License Options', 'bloom-french' ); ?></h2>
+			<h2><?php _e( 'Bloom French License Options', 'fxb-sample-translations' ); ?></h2>
 			<form method="post" action="options.php">
 
-				<?php settings_fields( 'fxb_bloom_license' ); ?>
+				<?php settings_fields( 'fxb_sample_license' ); ?>
 
 				<table class="form-table">
 					<tbody>
 					<?php echo do_action('fbx_key_setting', $license, $status, $content_key = ''); ?>
-					</tbody>	
+					</tbody>
 				</table>
 				<?php submit_button(); ?>
 
@@ -42,31 +42,31 @@ if(!function_exists('fxb_license_page')) {
 	}
 }
 
-function fbx_bloom_key($license, $status, $content_key) {
-	$plugin = 'Bloom';
+function fbx_sample_key($license, $status, $content_key) {
+	$plugin = 'FxB Sample';
 
 	$content_key .= '<tr valign="top">
 		<th scope="row" valign="top">'
-			. __( 'License Key', 'bloom-french' ) . ' ' . $plugin .'
+			. __( 'License Key', 'fxb-sample-translations' ) . ' ' . $plugin .'
 		</th>
 		<td>
-			<input id="fxb_bloom_license_key" name="fxb_bloom_license_key" type="text" class="regular-text" value="' . esc_attr__( $license ) . '" />
-			<label class="description" for="fxb_bloom_license_key">' . __( 'Enter your license key', 'bloom-french' ) . '</label>
+			<input id="fxb_sample_license_key" name="fxb_sample_license_key" type="text" class="regular-text" value="' . esc_attr__( $license ) . '" />
+			<label class="description" for="fxb_sample_license_key">' . __( 'Enter your license key', 'fxb-sample-translations' ) . '</label>
 		</td>
 	</tr>';
-	if ( false !== $license ) { 
+	if ( false !== $license ) {
 		$content_key .= '<tr valign="top">
 		<th scope="row" valign="top">
-			' . __( 'Activate License', 'bloom-french' ) . '
+			' . __( 'Activate License', 'fxb-sample-translations' ) . '
 		</th>
 		<td>';
 		if ( false !== $status && "valid" == $status ) {
-			$content_key .= '<span style="color:green;">' . __( 'active', 'bloom-french' ) . '</span>
-				' . wp_nonce_field( "fxb_bloom_nonce", "fxb_bloom_nonce" ) . '
-				<input type="submit" class="button-secondary" name="edd_license_deactivate" value="' . __( 'Deactivate License', 'bloom-french' ) .'"/>';
+			$content_key .= '<span style="color:green;">' . __( 'active', 'fxb-sample-translations' ) . '</span>
+				' . wp_nonce_field( "fxb_sample_nonce", "fxb_sample_nonce" ) . '
+				<input type="submit" class="button-secondary" name="edd_license_deactivate" value="' . __( 'Deactivate License', 'fxb-sample-translations' ) .'"/>';
 		} else {
-			$content_key .= wp_nonce_field( "fxb_bloom_nonce", "fxb_bloom_nonce" ) .'
-				<input type="submit" class="button-secondary" name="edd_license_activate" value="' . __( 'Activate License', 'bloom-french' ) .'"/>';
+			$content_key .= wp_nonce_field( "fxb_sample_nonce", "fxb_sample_nonce" ) .'
+				<input type="submit" class="button-secondary" name="edd_license_activate" value="' . __( 'Activate License', 'fxb-sample-translations' ) .'"/>';
 		}
 		$content_key .= '</td>
 		</tr>';
@@ -74,50 +74,50 @@ function fbx_bloom_key($license, $status, $content_key) {
 
 	echo $content_key;
 }
-add_action('fbx_key_setting', 'fbx_bloom_key', 10, 3);
+add_action('fbx_key_setting', 'fbx_sample_key', 10, 3);
 
-if(!function_exists('fxb_bloom_register_option')) {
-	function fxb_bloom_register_option() {
+if ( ! function_exists( 'fxb_sample_register_option' ) ) {
+	function fxb_sample_register_option() {
 		// creates our settings in the options table
-		register_setting( 'fxb_bloom_license', 'fxb_bloom_license_key', 'edd_sanitize_license' );
+		register_setting( 'fxb_sample_license', 'fxb_sample_license_key', 'edd_sanitize_license' );
 	}
-	add_action( 'admin_init', 'fxb_bloom_register_option' );
+	add_action( 'admin_init', 'fxb_sample_register_option' );
 }
 
-if(!function_exists('edd_sanitize_license')) {
+if ( ! function_exists( 'edd_sanitize_license' ) ) {
 	function edd_sanitize_license( $new ) {
-		$old = get_option( 'fxb_bloom_license_key' );
+		$old = get_option( 'fxb_sample_license_key' );
 		if ( $old && $old != $new ) {
-			delete_option( 'fxb_bloom_license_status' ); // new license has been entered, so must reactivate
+			delete_option( 'fxb_sample_license_status' ); // new license has been entered, so must reactivate
 		}
 		return $new;
 	}
 }
 
-if(!function_exists('fxb_bloom_activate_license')) {
-	function fxb_bloom_activate_license() {
+if ( ! function_exists( 'fxb_sample_activate_license' ) ) {
+	function fxb_sample_activate_license() {
 
 		// listen for our activate button to be clicked
 		if ( isset( $_POST['edd_license_activate'] ) ) {
 
 			// run a quick security check
-			if ( ! check_admin_referer( 'fxb_bloom_nonce', 'fxb_bloom_nonce' ) ) {
+			if ( ! check_admin_referer( 'fxb_sample_nonce', 'fxb_sample_nonce' ) ) {
 				return; // get out if we didn't click the Activate button
 			}
 
 			// retrieve the license from the database
-			$license = trim( get_option( 'fxb_bloom_license_key' ) );
+			$license = trim( get_option( 'fxb_sample_license_key' ) );
 
 			// data to send in our API request
 			$api_params = array(
 				'edd_action' => 'activate_license',
 				'license'   => $license,
-				'item_name' => urlencode( FXB_BLOOM_ITEM_NAME ), // the name of our product in EDD
+				'item_name' => urlencode( FXB_SAMPLE_ITEM_NAME ), // the name of our product
 				'url'       => home_url()
 			);
 
 			// Call the custom API.
-			$response = wp_remote_get( add_query_arg( $api_params, FXB_BLOOM_STORE_URL ), array( 'timeout' => 15, 'sslverify' => false ) );
+			$response = wp_remote_get( add_query_arg( $api_params, FXB_SAMPLE_STORE_URL ), array( 'timeout' => 15, 'sslverify' => false ) );
 
 			// make sure the response came back okay
 			if ( is_wp_error( $response ) ) {
@@ -129,26 +129,26 @@ if(!function_exists('fxb_bloom_activate_license')) {
 
 			// $license_data->license will be either "valid" or "invalid"
 
-			update_option( 'fxb_bloom_license_status', $license_data->license );
+			update_option( 'fxb_sample_license_status', $license_data->license );
 
 		}
 	}
-	add_action( 'admin_init', 'fxb_bloom_activate_license' );
+	add_action( 'admin_init', 'fxb_sample_activate_license' );
 }
 
-if(!function_exists('fxb_bloom_deactivate_license')) {
-	function fxb_bloom_deactivate_license() {
+if ( ! function_exists( 'fxb_sample_deactivate_license' ) ) {
+	function fxb_sample_deactivate_license() {
 
 		// listen for our activate button to be clicked
 		if ( isset( $_POST['edd_license_deactivate'] ) ) {
 
 			// run a quick security check
-			if ( ! check_admin_referer( 'fxb_bloom_nonce', 'fxb_bloom_nonce' ) ) {
+			if ( ! check_admin_referer( 'fxb_sample_nonce', 'fxb_sample_nonce' ) ) {
 				return; // get out if we didn't click the Activate button
 			}
 
 			// retrieve the license from the database
-			$license = trim( get_option( 'fxb_bloom_license_key' ) );
+			$license = trim( get_option( 'fxb_sample_license_key' ) );
 
 			// data to send in our API request
 			$api_params = array(
@@ -171,49 +171,48 @@ if(!function_exists('fxb_bloom_deactivate_license')) {
 
 			// $license_data->license will be either "deactivated" or "failed"
 			if ( 'deactivated' == $license_data->license ) {
-				delete_option( 'fxb_bloom_license_status' );
+				delete_option( 'fxb_sample_license_status' );
 			}
 		}
 	}
-	add_action( 'admin_init', 'fxb_bloom_deactivate_license' );
+	add_action( 'admin_init', 'fxb_sample_deactivate_license' );
 }
 
-if(!function_exists('fxb_bloom_init')) {
+if ( ! function_exists( 'fxb_sample_init' ) ) {
 	/* Hook to init
 	------------------------------------------*/
-	add_action( 'init', 'fxb_bloom_init' );
+	add_action( 'init', 'fxb_sample_init' );
 
 	/**
 	 * Add the text domain to init.
 	 *
 	 * @since 1.0
 	 */
-	function fxb_bloom_init() {
+	function fxb_sample_init() {
 
 		/* Language */
 		// Set filter for language directory
 		$lang_dir = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
-		$lang_dir = apply_filters( 'fxb_bloom_languages_directory', $lang_dir );
+		$lang_dir = apply_filters( 'fxb_sample_languages_directory', $lang_dir );
 
 		// Traditional WordPress plugin locale filter
-		$locale = apply_filters( 'plugin_locale', get_locale(), 'bloom' );
-		$mofile = sprintf( '%1$s-%2$s.mo', 'bloom', $locale );
+		$locale = apply_filters( 'plugin_locale', get_locale(), 'fxb-sample' );
+		$mofile = sprintf( '%1$s-%2$s.mo', 'fxb-sample', $locale );
 
 		// Setup paths to current locale file
 		$mofile_local   = $lang_dir . $mofile;
-		$mofile_global  = WP_LANG_DIR . '/bloom-french/' . $mofile;
+		$mofile_global  = WP_LANG_DIR . 'fxb-sample/' . $mofile;
 
 		if ( file_exists( $mofile_global ) ) {
-			// Look in global /wp-content/languages/bloom-french/ folder
-			load_textdomain( 'bloom', $mofile_global );
+			// Look in global /wp-content/languages/fxb-sample/ folder
+			load_textdomain( 'fxb-sample', $mofile_global );
 		} elseif ( file_exists( $mofile_local ) ) {
-			// Look in local /wp-content/plugins/bloom-french/languages/ folder
-			load_textdomain( 'bloom', $mofile_local );
+			// Look in local /wp-content/plugins/fxb-sample/languages/ folder
+			load_textdomain( 'fxb-sample', $mofile_local );
 		} else {
 			// Load the default language files
-			load_plugin_textdomain( 'bloom', false, $lang_dir );
-					load_plugin_textdomain( 'et_dashboard', false, $lang_dir );
-
+			load_plugin_textdomain( 'fxb-sample', false, $lang_dir );
+					load_plugin_textdomain( 'fxb-sample', false, $lang_dir );
 
 		}
 	}
