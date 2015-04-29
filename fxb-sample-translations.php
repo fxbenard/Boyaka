b68e2971f5e11da4cf91734daff10749d06f1f85
fxbenard <fxbenard@gmail.com>
2015-04-29 09:40:35 +0200

    remove unused class

--- fxb-sample-translations.php
+++ fxb-sample-translations.php
@@ -215,10 +215,8 @@ if ( ! class_exists( 'FXB_SAMPLE_Plugin' ) ) {
  *              present.
  */
 function FXB_SAMPLE_Plugin_load() {
-	if ( ! class_exists( 'Easy_Digital_Downloads' ) ) {
 		if ( ! class_exists( 'EDD_Extension_Activation' ) ) {
 			require_once 'includes/class.extension-activation.php';
-		}
 
 		$activation = new EDD_Extension_Activation( plugin_dir_path( __FILE__ ), basename( __FILE__ ) );
 		$activation = $activation->run();
